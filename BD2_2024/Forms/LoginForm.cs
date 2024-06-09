f91421c1b447b605de5d8c4b8a367c99cb6c7719
Thiago Giebmeyer <thiagogiebmeyer.visualsoftware@gmail.com>
2024-06-09 18:11:56 -0300

    Conexão com o banco Postgres.
    Criado arquivo de conexão com o banco de dados e realizado configuração através do login do usuario.

--- BD2_2024/Forms/LoginForm.cs
+++ BD2_2024/Forms/LoginForm.cs
@@ -1,6 +1,8 @@
 ﻿using BD2_2024.Forms;
+using PostgresConnectionExample;
 using System;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace BD2_2024
 {
@@ -19,6 +21,7 @@ namespace BD2_2024
 
         private void Login_Load(object sender, EventArgs e)
         {
+
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
@@ -36,10 +39,22 @@ namespace BD2_2024
             } 
             else
             {
-                this.Hide();
+                try
+                {
+                    var connectionString = $"Host=127.0.0.1;Username={txtUsuario};Password={txtSenha};Database=mySales";
+                    var database = DatabasePostgresConnection.GetInstance(connectionString);
+                    
+                    this.Hide();
 
-                Form mainForm = new MainForm();
-                mainForm.Show();
+                    Form mainForm = new MainForm();
+                    mainForm.Show();
+                }
+                catch (Exception ex)
+                {
+                    showMessageBox($"Erro ao tentar inicializar a conexão: {ex.Message}");
+                }
+
+                
             }
 
         }
--- BD2_2024/Forms/MainForm.cs
+++ BD2_2024/Forms/MainForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Npgsql;
+using PostgresConnectionExample;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +19,26 @@ namespace BD2_2024.Forms
             InitializeComponent();
         }
 
+        private void showMessageBox(String txt, String title = "Atenção!")
+        {
+            MessageBox.Show(txt, title);
+        }
+
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            using (var connection = DatabasePostgresConnection.GetInstance().GetConnection())
+            {
+                using (var command = new NpgsqlCommand("SELECT version()", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            showMessageBox(reader.GetString(0));
+                        }
+                    }
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
